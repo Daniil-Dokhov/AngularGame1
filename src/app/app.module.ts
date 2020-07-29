14748d21651ab7c5149fd7509e67d3d6f288d66b
BIG-DAN-SO-BIG <DohovDO@mail.ru>
2020-07-29 15:23:44 +0300

    Create basic for game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+import { FieldComponent } from './field/field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    FieldComponent,
   ],
   imports: [
     BrowserModule
